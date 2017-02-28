dcdfcf5ff12a9c6076787ebe8ce28a3151c17a26
chao.wang <chao.wang>
2017-02-28 16:08:36 +0800

    Initial commit

---
+++ app/src/main/java/com/eascs/testrecycleview/MainActivity.java
@@ -0,0 +1,54 @@
+package com.eascs.testrecycleview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView rclv;
+    //一步步添加，首先需要添加Adapter
+    private myAdapter adapter;
+    private List<String> listData = new ArrayList<String>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initData();
+        rclv = (RecyclerView) findViewById(R.id.rclv_Test);
+        rclv.setLayoutManager(new GridLayoutManager(this, 2));
+        rclv.setItemAnimator(new DefaultItemAnimator());
+        adapter = new myAdapter(MainActivity.this, listData);
+        rclv.setAdapter(adapter);
+//
+//        adapter.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//
+//                Toast.makeText(MainActivity.this, "点击了" + position, Toast.LENGTH_SHORT).show();
+//
+//            }
+//        });
+    }
+
+    private void initData() {
+
+        listData.add("我的单据");
+
+        listData.add("历史单据");
+
+        listData.add("订单统计");
+
+        listData.add("基础信息");
+
+    }
+}
---
+++ app/src/main/java/com/eascs/testrecycleview/myAdapter.java
@@ -0,0 +1,95 @@
+package com.eascs.testrecycleview;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+/**
+ * Created by chao.wang on 2017/2/28.
+ */
+
+public class myAdapter extends RecyclerView.Adapter<myAdapter.ViewHolder> {
+
+
+    private Context mContext;
+    private List<String> mDataSet;
+    private AdapterView.OnItemClickListener onItemClickListener;
+    private AdapterView.OnLongClickListener onLongClickListener;
+
+    public myAdapter(Context context, List<String> dataSet) {
+        mContext = context;
+        mDataSet = dataSet;
+    }
+
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(mContext).inflate(R.layout.layout_list_item, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+
+        holder.text.setText(mDataSet.get(position));
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(mContext, "点击了" + position, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+
+                Toast.makeText(mContext, "wangchao" , Toast.LENGTH_SHORT).show();
+
+                //这个位置一定要注意，返回值是true 和false是
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return mDataSet.size();
+
+    }
+
+    public void remove(int position) {
+
+        mDataSet.remove(position);
+        notifyItemRemoved(position);
+
+    }
+
+    public void add(String text, int position) {
+
+        mDataSet.add(position, text);
+        notifyItemInserted(position);
+
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView text;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            text = (TextView) itemView.findViewById(R.id.text);
+        }
+    }
+
+
+}
